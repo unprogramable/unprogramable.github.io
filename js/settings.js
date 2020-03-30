fb86b7f0501ab26f0ca831959a5eccd15e4d7a44
Unprogramable <unprogramable@gmail.com>
2020-03-30 12:15:45 +0430

    better view

--- js/settings.js
+++ js/settings.js
@@ -3,15 +3,15 @@ $(function(){
 
         delay:0,                   // start delay in seconds [default:0]
 
-        speed:60,                  // typing speed (ms) [default:50]
+        speed:80,                  // typing speed (ms) [default:50]
         speed_vary:false,          // 'human like' speed variation [default:false]
 
         beep:false,                 // beep while typing (Web Audio API) [default:false]
 
-        mistype:3,                 // mistype rate: 1:N per char [default:false]
+        mistype:false,                 // mistype rate: 1:N per char [default:false]
         locale:'en',               // keyboard layout (to fit mistype); supported: 'en' (english) or 'de' (german) [default:'en']
 
-        caret:'<span style="font-size:2em;">\u258e</span>',            // caret content; can be html too [default:true (\u258e)]
+        caret:'\u258e',            // caret content; can be html too [default:true (\u258e)]
         blink:true,                // blink-interval in ms; if TRUE, speed*3  [default:true]
         blink_perm:false,          // permanent blinking? if FALSE, caret blinks only on delay/pause/finish [default:false]
         repeat:0,                  // repeat typing: if TRUE, infinite or N times [default:0]
