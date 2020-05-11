e7f2ad2ff5f95804225d8ab6fe8beaec64ba9993
unprogramable <unprogramable@gmail.com>
2020-05-12 03:35:16 +0430

    bug fixes 3

--- js/settings.js
+++ js/settings.js
@@ -1,17 +1,23 @@
+
 if (! sessionStorage.clickcount) {
     sessionStorage.clickcount = 1;
 }
 
-if(sessionStorage.clickcount <= 3){
-    board='board'+sessionStorage.clickcount;
+var board_count = 3;
+var visit_count = sessionStorage.clickcount;
+if(visit_count <= board_count){
+
+    board='board'+visit_count;
     document.getElementById(board).style.display = "block";
-    sessionStorage.clickcount = Number(sessionStorage.clickcount) + 1;
-    if(sessionStorage.clickcount == 4 )
+    sessionStorage.clickcount = Number(visit_count) + 1;
+    if(visit_count == board_count){
         sessionStorage.removeItem('clickcount');
+    }
 }
 
+
 document.getElementById(board).style["padding-bottom"]="100px";
-document.write("Unprogramable");
+document.write("<p style='color:red;'>unprogramable</p><br/><br/>");
 
 $(function(){
 
@@ -32,7 +38,7 @@ $(function(){
         repeat:0,                  // repeat typing: if TRUE, infinite or N times [default:0]
         tag:'span',                // wrapper tag (.t-container, .t-caret) [default:'span']
         pause_on_click:false,       // pauses/continues typing on click/tap (elm) [default:false]
-        pause_on_tab_switch:true,  // pauses typing if window is inactive (Page Visibility API) [default:false]
+        pause_on_tab_switch:false,  // pauses typing if window is inactive (Page Visibility API) [default:false]
 
         // init callback (ready-to-type)
         init:function(elm){},
